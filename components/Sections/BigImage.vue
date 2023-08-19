0ca38b91df4eec9d4dafe7376991d916152955cc
Albert Ferrate <albert@ferrate.cat>
2023-08-19 12:31:47 +0200

    Initial Commit

---
+++ components/Sections/BigImage.vue
@@ -0,0 +1,19 @@
+<template>
+  <Container>
+    <div class="relative mx-auto overflow-hidden shadow-lg">
+      <img
+        src="~/assets/img/ijen_pano_2.png"
+        class="h-2/3 w-full object-fill"
+      />
+      <div
+        class="absolute left-1/2 bottom-12 h-full w-full rotate-45 scale-110 bg-white opacity-100"
+      ></div>
+      <div
+        class="absolute left-1/2 bottom-0 h-full w-full -rotate-45 scale-150 bg-contrast-1 opacity-80"
+      ></div>
+      <div class="absolute right-0 bottom-12 bg-white p-3 text-2xl">
+        Bromo Ijen Indonesia Tours
+      </div>
+    </div>
+  </Container>
+</template>
