a1203ed9691b7ecfce2bd9728022cdd5deb74ff9
Albert Ferrate <albert@ferrate.cat>
2023-08-20 18:37:03 +0200

    Added Footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,3 +1,18 @@
 <template>
-  <div class="mt-16"></div>
+  <p
+    class="mt-16 flex items-center justify-center text-justify text-base text-gray-500"
+  >
+    Made with
+    <HeartIcon class="mx-1.5 inline h-5 w-5" /> by
+    <a href="https://albertferrate.com" target="_blank" class="ml-1 underline"
+      >Albert Ferraté</a
+    >
+  </p>
+  <p class="mt-2 flex justify-center pb-8 text-justify text-sm text-gray-500">
+    © 2023. All rights reserved.
+  </p>
 </template>
+
+<script setup>
+import { HeartIcon } from "@heroicons/vue/20/solid";
+</script>
