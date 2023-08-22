431f014a6a4f25991c071e0c1270f5fb35dbc162
Albert Ferrate <albert@ferrate.cat>
2023-08-22 21:22:59 +0200

    Updated G-Tag

--- plugins/vue-gtag.client.js
+++ plugins/vue-gtag.client.js
@@ -2,7 +2,7 @@ import VueGtag from "vue-gtag-next";
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(VueGtag, {
     property: {
-      id: "G-DLS14TS3RC",
+      id: "G-S8NFLNZWST",
     },
   });
 });
